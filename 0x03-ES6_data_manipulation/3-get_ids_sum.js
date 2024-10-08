841b599a01c19959028f06a0715d1b50cf5412c2
kazeneza-zephilin <kzephilin2@gmail.com>
2024-10-09 01:44:04 +0200

    added data manipulation file

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentIdsSum(studentList = []){
-    return studentList.reduce((prev, current) => prev + current.id, 0);
+export default function getStudentIdsSum(studentList = []) {
+  return studentList.reduce((prev, current) => prev + current.id, 0);
 }
